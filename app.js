b8efbdbeb9e49cd49b03d73a9bc77fe50428f274
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-20 11:12:44 +0530

    Creating Connection

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 require('dotenv').config()
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 //Setting static forlder
 app.use(express.static(path.join(__dirname,'public')))
 
+//Run when client connects
+io.on('connection', socket => {
+    //welcome user
+    socket.emit('message','Welcome to ChatCord!' )
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat')
+
+//Run when client disconnects
+     socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+     })
+})
+
+
+
 
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
+server.listen(PORT, () => console.log(`Server is running at ${PORT}`))
